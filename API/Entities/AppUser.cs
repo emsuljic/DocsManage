4b3d444e3bf1cdbad532b80b3b6ab4ae950b0a31
emsuljic <esuljic5@gmail.com>
2021-05-11 16:02:18 +0200

    End of section 1

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string WorkplaceDescription { get; set; }
+
+    }
+}
