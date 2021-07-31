9a2f893e02a8eaafdb21ff097e1bc7f2e8984f4e
emsuljic <esuljic5@gmail.com>
2021-07-31 15:41:52 +0200

    Section 5

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdministrationComponent } from './administration/administration.component';
+import { HomeComponent } from './home/home.component';
+import { DocumentsComponent } from './IOdocuments/documents/documents.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ProtocolBookComponent } from './protocol-book/protocol-book.component';
+import { AuthGuard } from './_guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
+      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'protocolBook', component: ProtocolBookComponent },
+      { path: 'documents', component: DocumentsComponent },
+      { path: 'administration', component: AdministrationComponent },
+    ]
+  },
+
+  { path: '**', component: HomeComponent, pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ client/src/app/protocol-book/protocol-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-protocol-book',
+  templateUrl: './protocol-book.component.html',
+  styleUrls: ['./protocol-book.component.css']
+})
+export class ProtocolBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
